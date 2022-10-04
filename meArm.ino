5a0d6675df19683b0620c68c9f3dab9e2a1eae49
Lin Yu-Chieh (Victor) <victorlin@gapps.ntust.edu.tw>
2022-10-04 18:16:17 +0800

    Add files via upload

--- meArm.ino
+++ meArm.ino
@@ -1,44 +1,31 @@
 #include <Servo.h>
-String str = "90,90,90,90";//120 to closeGripper
-int angle[]={90,90,90,90};
-Servo s[4];
-
+String str = "090090090";
+String temp = "";
+int a, b, c, d;
+Servo base, left, right, grip;
 void setup() {
-  s[0].attach(11);//direction
-  s[1].attach(10);//right
-  s[2].attach(9);//left
-  s[3].attach(6);//gripper
+  base.attach(11); 
+  left.attach(10);
+  right.attach(9);
+  grip.attach(6);
   Serial.begin(9600);
-  Serial.setTimeout(5);
+  Serial.setTimeout(10);
 }
+
 void loop() {
   if (Serial.available() > 0) {
     str = Serial.readString();
-    //Serial.print("\r\ninput:"+str);
-    int i = 0;
-    int temp =0;
-    for(int j =0; j< str.length(); j++){
-      if(isdigit(str[j])){
-        temp*=10;
-        temp+=String(str[j]).toInt();
-        //Serial.print("\r\ni value:"+String(i)+" j value:"+String(j)+" temp value:"+String(temp));
-      }
-      else if(str[j]==','||j==str.length()-1){
-        //Serial.print("\r\n------------");
-        angle[i]=temp;
-        temp=0;
-        i++;
-        if(i>=4) break;
-      }
-      else continue;
-    }
-    if(str[0]!='E'){
-      s[0].write(angle[0]);
-      s[1].write(angle[1]);
-      s[2].write(angle[2]);
-      s[3].write(angle[3]);
-    }
-    //Serial.print(+"\r\n"+String(s[0].read())+", "+String(s[1].read())+", "+String(s[2].read())+", "+String(s[3].read())+"\r\n");
+    temp = str.substring(0, 3);
+    a = temp.toInt();
+    temp = str.substring(3, 6);
+    b = temp.toInt();
+    temp = str.substring(6, 9);
+    c = temp.toInt();
+    temp = str.substring(9, 12);
+    d = temp.toInt();
+    base.write(a);
+    left.write(b);
+    right.write(c);
+    grip.write(d);
   }
-  delay(100);
 }
